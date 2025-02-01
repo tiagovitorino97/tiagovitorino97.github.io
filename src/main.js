9f1aaf76402d7a2608962b989982ced4c76dc611
tiagovitorino97 <tiagomsvitorino@gmail.com>
2025-02-01 18:36:00 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.155/build/three.module.js';
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.155/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.155/examples/jsm/controls/OrbitControls.js';
 
-// Patch the modules to use the CDN version of THREE
-GLTFLoader.prototype.constructor.prototype = THREE;
-OrbitControls.prototype.constructor.prototype = THREE;
+const { GLTFLoader } = await import('https://cdn.jsdelivr.net/npm/three@0.155/examples/jsm/loaders/GLTFLoader.js');
+const { OrbitControls } = await import('https://cdn.jsdelivr.net/npm/three@0.155/examples/jsm/controls/OrbitControls.js');
 
 
 // Constants for reusable values
